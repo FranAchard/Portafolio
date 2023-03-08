a814d2e44165891ce7224d719536640cf34e7832
FranAchard <franachard8@gmail.com>
2023-03-08 07:56:46 -0300

    Home casi{

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import "./Home.css"
+import "./Home.css";
+import perfil from "./perfil.png"
 const Home =()=>{
     return(
         <div>
             <div className="title"><h1>I'm a Full Stack Developer</h1></div>
-            <div className="text"><p>Estoy buscando oportunidades como desarrollador Full Stack con experiencia en JavaScript, React, Redux, Bootstrap, HTML, CSS, Node.js, SQL y MongoDB. Tengo habilidades para crear y desarrollar aplicaciones web completas. Si está interesado en trabajar juntos o conoce alguna oportunidad adecuada, no dude en ponerse en contacto conmigo a través de LinkedIn.</p></div>
+            <div className="container2">
+            <div className="text"><p>Estoy en la búsqueda de posibilidades como programador Full Stack con habilidades en JavaScript, React, Redux, Bootstrap, HTML, CSS, Node.js, SQL y MongoDB. Poseo experiencia en el diseño y desarrollo de aplicaciones web integrales. Si está interesado en colaborar conmigo o conoce de alguna oportunidad pertinente, no dude en contactarme mediante mi perfil en LinkedIn.</p></div>
+            <div><img src={perfil} alt="Francisco Achard" className="perfil"></img></div> </div>
         </div>
     )
 }
